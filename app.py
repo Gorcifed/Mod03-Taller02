9d868037f324e9a1122daba24a31d6d447047c5a
e.gordillo <e.gordillo@ciel.corp>
2024-11-27 20:30:05 -0500

    ajuste

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 from controllers.controller_animal import animales_blueprint
 import os
 
-app = Flask(__name__, template_folder = "Views")
+app = Flask(__name__, template_folder = "views")
 app.register_blueprint(animales_blueprint)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("Index.html")
 
 
 if __name__ == '__main__':
